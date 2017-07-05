bfd6ba32490733ead0e23e4f89704dcd78e64d70
jamessafko <jamessafko@gmail.com>
2017-07-05 11:04:24 -0400

    Adding Serial init button

--- MyFirstApp/app/src/main/java/com/example/juanv/sample/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/juanv/sample/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.juanv.sample;
 import android.content.Context;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +11,6 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
-import com.devadvance.circularseekbar.CircularSeekBar;
 import java.io.IOException;
 import java.util.List;
 import usbserial.driver.UsbSerialDriver;
@@ -22,6 +19,7 @@ import usbserial.driver.UsbSerialProber;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Global Variables
     private static final String TAG = "MainActivity";
     public UsbSerialPort port = null;
     private SeekBar volumeSeekbar = null;
@@ -35,41 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         myText = (TextView) findViewById(R.id.textView5);
 
-
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-        if (availableDrivers.isEmpty()) {
-            Log.d(TAG, "chosen juan 2");
-            myText.setText(" driver empty\n");
-            return;
-        }
-
-        // Open a connection to the first available driver.
-        UsbSerialDriver driver = availableDrivers.get(0);
-        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-        if (connection == null) {
-            myText.setText(" null connection");
-            return;
-            // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
-        }
-
-        // Read some data! Most have just one port (port 0).
-        port = driver.getPorts().get(0);
-        //Log.d(TAG, "chosen juan 4");
-        myText.setText(" connected before try\n");
-
-        try {
-            port.open(connection);
-            port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-            myText.append("connected, port open in try\n");
-
-        } catch (IOException e) {
-            myText.append(" caught an error in port open");
-            e.printStackTrace();
-
-        }
-
-        initSliderControls(port);
+        serialButtonInit();//initialize serial comms
+        serialSliderInit(port);//initialize button
     }
 
     //Enable full screen mode
@@ -98,8 +63,50 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Code for initializing serial communications
+    private void serialButtonInit(){
+        Button serialButton = (Button) findViewById(R.id.serialComms);
+
+        serialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+                List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+                if (availableDrivers.isEmpty()) {
+                    Log.d(TAG, "chosen juan 2");
+                    myText.setText(" driver empty\n");
+                    return;
+                }
+
+                // Open a connection to the first available driver.
+                UsbSerialDriver driver = availableDrivers.get(0);
+                UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+                if (connection == null) {
+                    myText.setText(" null connection");
+                    return;
+                    // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
+                }
+
+                // Read some data! Most have just one port (port 0).
+                port = driver.getPorts().get(0);
+                //Log.d(TAG, "chosen juan 4");
+                myText.setText(" connected before try\n");
+
+                try {
+                    port.open(connection);
+                    port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+                    myText.append("connected, port open in try\n");
+
+                } catch (IOException e) {
+                    myText.append(" caught an error in port open");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
     //Code for slider change
-    private void initSliderControls(final UsbSerialPort port){
+    private void serialSliderInit(final UsbSerialPort port){
         try {
             volumeSeekbar = (SeekBar)findViewById(R.id.volume);
             volumeSeekbar.setMax(100);
@@ -117,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
                     byte value = (byte)(volumeSeekbar.getProgress() & 0xFF);
                     byte outBuffer[] = {value};
-                    myText.setText("Value is " + value);
+                    myText.setText("Value is " + value + "\n");
                     sendserial(port, outBuffer);
                 }
             });
