f64e033c65b50c3928000148370101c20e1f1c29
jamessafko <jamessafko@gmail.com>
2017-07-12 08:58:38 -0400

    Full Screening Main menu

--- Android Studio/MAIN/app/src/main/java/com/example/kfalasiri/mixers/Launcher.java
+++ Android Studio/MAIN/app/src/main/java/com/example/kfalasiri/mixers/Launcher.java
@@ -16,6 +16,8 @@ public class Launcher extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
 
+        fullScreen();
+
         Button button1 = (Button) findViewById(R.id.button);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,35 @@ public class Launcher extends AppCompatActivity {
             }
         });
 
+    }
+
+    /////////////////////////////////////////Setting up views//////////////////////////////////////
+    //Enable full screen mode
+
+    public void fullScreen(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocas) {
+        super.onWindowFocusChanged(hasFocas);
+        View decorView = getWindow().getDecorView();
+        if(hasFocas) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 
 }
--- Android Studio/MAIN/app/src/main/java/com/example/kfalasiri/mixers/MainActivity.java
+++ Android Studio/MAIN/app/src/main/java/com/example/kfalasiri/mixers/MainActivity.java
@@ -22,12 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setupViewPager(viewPager);//add the fragments to the viewPager
         viewPager.setPagingEnabled(false);//turn off swiping to move between tabs
 
+        //Fullscreen
+        fullScreen();
+
         //TabLayout
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 
     /////////////////////////////////////////Setting up views//////////////////////////////////////
+    public void fullScreen(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
     //Enable full screen mode
     @Override
     public void onWindowFocusChanged(boolean hasFocas) {
