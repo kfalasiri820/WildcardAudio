c4c3f558f6e6cf90d5f33934c261a4adabc633a3
jamessafko <jamessafko@gmail.com>
2017-07-26 11:16:27 -0400

    Updating Tap function

---
+++ Code Composer/MAIN/Debug/Effects/oldPhaser.d
@@ -0,0 +1,63 @@
+# FIXED
+
+Effects/oldPhaser.obj: ../Effects/oldPhaser.c
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP28x_Project.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Device.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Adc.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_DevEmu.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_CpuTimers.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECan.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECap.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_DMA.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_EPwm.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_EQep.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Gpio.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_I2c.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Mcbsp.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieCtrl.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieVect.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Spi.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Sci.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_SysCtrl.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_XIntrupt.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Xintf.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_Examples.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_GlobalPrototypes.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_EPwm_defines.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_Dma_defines.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_I2c_defines.h
+Effects/oldPhaser.obj: /Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_DefaultIsr.h
+Effects/oldPhaser.obj: /Applications/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/math.h
+Effects/oldPhaser.obj: /Applications/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/linkage.h
+Effects/oldPhaser.obj: /Applications/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/_defs.h
+
+../Effects/oldPhaser.c: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP28x_Project.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Device.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Adc.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_DevEmu.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_CpuTimers.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECan.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECap.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_DMA.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_EPwm.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_EQep.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Gpio.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_I2c.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Mcbsp.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieCtrl.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieVect.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Spi.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Sci.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_SysCtrl.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_XIntrupt.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/headers/include/DSP2833x_Xintf.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_Examples.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_GlobalPrototypes.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_EPwm_defines.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_Dma_defines.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_I2c_defines.h: 
+/Applications/ti/c2000/C2000Ware_1_00_00_00/device_support/f2833x/common/include/DSP2833x_DefaultIsr.h: 
+/Applications/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/math.h: 
+/Applications/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/linkage.h: 
+/Applications/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/_defs.h: 
